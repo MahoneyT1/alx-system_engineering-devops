9da5be91e44a9bb8b2821c5405b189d56c97ffc8
Your Name <ifeanyitech15@gmail.com>
2024-04-06 04:20:14 +0000

    ssl

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,12 @@
-#!/usr/bin/pup
-# Using Puppet, install flask from pip3.
+package { 'python3-pip':
+  ensure => installed,
+}
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
+  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  environment => ['PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  require     => Package['python3-pip'],
 }
 
