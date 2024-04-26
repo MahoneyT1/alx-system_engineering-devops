12a891f524a25316b51039fd2c742c23d04102b6
Your Name <ifeanyitech15@gmail.com>
2024-04-26 05:35:17 +0000

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,64 +9,67 @@ Requirements:
 - Display progress information in the specified format
 """
 
-from sys import argv
-import json
-import urllib.request
+if __name__ == "__main__":
 
-# get the commandline arg
-argv_user_id = int(argv[1])
+    from sys import argv
+    import json
+    import urllib.request
 
-# build a url to get the userID witht the argv_user_id
-url_employee_id = f"https://jsonplaceholder.typicode.com/todos/{argv_user_id}"
+    # get the commandline arg
+    argv_user_id = int(argv[1])
 
-# set counter for tasks
-count_all_task_for_argv = 0
-NUMBER_OF_DONE_TASKS = 0
-TASK_TITLE = ""
+    # build a url to get the userID witht the argv_user_id
+    u = f"https://jsonplaceholder.typicode.com/todos/{argv_user_id}"
+    url_employee_id = u
 
-# extract user id
-with urllib.request.urlopen(url_employee_id) as response:
-    data_todo = json.loads(response.read().decode())
+    # set counter for tasks
+    count_all_task_for_argv = 0
+    NUMBER_OF_DONE_TASKS = 0
+    TASK_TITLE = ""
 
-    # extract user name
-    user_id = data_todo['id']
-
-    # build a url with id gotten
-    url_user_name = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-
-    with urllib.request.urlopen(url_user_name) as res:
-        data_usernames = json.loads(res.read().decode())
-
-        # store the argv user name to in var
-        EMPLOYEE_NAME = data_usernames['name']
-
-    # Task counter session
-    url_count_tasks = f"https://jsonplaceholder.typicode.com/todos"
-
-    with urllib.request.urlopen(url_count_tasks) as response:
+    # extract user id
+    with urllib.request.urlopen(url_employee_id) as response:
         data_todo = json.loads(response.read().decode())
 
-        # loop through data_todo and increment task
-        for cont in range(len(data_todo)):
-            if data_todo[cont]['userId'] == argv_user_id:
-                count_all_task_for_argv += 1
+        # extract user name
+        user_id = data_todo['id']
 
-        TOTAL_NUMBER_OF_TASKS = count_all_task_for_argv
+        # build a url with id gotten
+        url_user_name = f"https://jsonplaceholder.typicode.com/users/{user_id}"
 
-        for cont in range(len(data_todo)):
-            if data_todo[cont]['userId'] == argv_user_id and \
-                    data_todo[cont]['completed']\
-                    is True:
-                NUMBER_OF_DONE_TASKS += 1
+        with urllib.request.urlopen(url_user_name) as res:
+            data_usernames = json.loads(res.read().decode())
 
-        print("Employee EMPLOYEE_NAME is done with tasks({}/{})".format(
-               NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+            # store the argv user name to in var
+            EMPLOYEE_NAME = data_usernames['name']
 
-        # print(TOTAL_NUMBER_OF_TASKS)
-        for cont in range(len(data_todo)):
-            if data_todo[cont]['userId'] == argv_user_id\
+        # Task counter session
+        url_count_tasks = f"https://jsonplaceholder.typicode.com/todos"
+
+        with urllib.request.urlopen(url_count_tasks) as response:
+            data_todo = json.loads(response.read().decode())
+
+            # loop through data_todo and increment task
+            for cont in range(len(data_todo)):
+                if data_todo[cont]['userId'] == argv_user_id:
+                    count_all_task_for_argv += 1
+
+            TOTAL_NUMBER_OF_TASKS = count_all_task_for_argv
+
+            for cont in range(len(data_todo)):
+                if data_todo[cont]['userId'] == argv_user_id and \
+                        data_todo[cont]['completed']\
+                        is True:
+                    NUMBER_OF_DONE_TASKS += 1
+
+            print("Employee EMPLOYEE_NAME is done with tasks({}/{})".format(
+                   NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+
+            # print(TOTAL_NUMBER_OF_TASKS)
+            for cont in range(len(data_todo)):
+                if data_todo[cont]['userId'] == argv_user_id\
                                             and data_todo[cont]['completed']\
                                             is True:
-                NUMBER_OF_DONE_TASKS += 1
-                TASK_TITLE = data_todo[cont]['title']
+                    NUMBER_OF_DONE_TASKS += 1
+                    TASK_TITLE = data_todo[cont]['title']
                 print("\t", TASK_TITLE)
