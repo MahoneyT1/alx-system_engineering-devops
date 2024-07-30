ac816d9bc6aa2022763c775fc97c62027208db35
MahoneyT1 <ifeanyitech15@gmail.com>
2024-07-30 16:52:47 +0100

    upload

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,23 +5,37 @@ TODO list progress.
 """
 
 import requests
-import sys.argv
+from sys import argv
 
 
 # extract employee_id from commandline
 employee_id = int(argv[1])
-
+base_endpoint = 'https://jsonplaceholder.typicode.com'
 
 if employee_id:
-    # employee_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
-    todo_url = 'https://jsonplaceholder.typicode.com/todos/{}'.format(employee_id)
+    todo_url = '{}/todos?userId={}'.format(base_endpoint, employee_id)
+    username_url = '{}/users/{}'.format(base_endpoint, employee_id)
+
+    # make todo request
+    todo_response = requests.get(todo_url)
+    json_data = todo_response.json()
     
-    # make a request
-    response = requests.get(todo_url)
+    # makae user request
+    username_response = requests.get(username_url)
+    username_json = username_response.json()
 
-    # jsonify response body
-    json_data = response.json()
-    completed_task = None
+    # User passed as an arg / todo task completed
+    completed_task = 0
+    not_completed_task = 0
+    
 
-    print(json_data) 
+    for item in json_data:
+        count = 0
+        if 'completed' in item and item.get('completed') is True:
+            completed_task += 1
+        not_completed_task += 1
 
+    # extract username
+    username = 
+
+    print(completed_task, '/', not_completed_task)
