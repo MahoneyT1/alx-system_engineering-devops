4f4943f85562697c1e215a6fbc43568a01389795
MahoneyT1 <ifeanyitech15@gmail.com>
2024-07-31 11:31:50 +0100

    wrote to a csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,63 +1,24 @@
 #!/usr/bin/python3
-'''
-gather employee data from API
-'''
-
-from sys import argv
+""" Export api to csv"""
 import csv
 import requests
-
-base_endpoint = 'https://jsonplaceholder.typicode.com'
+import sys
 
 if __name__ == '__main__':
-    if len(argv) <= 2:
-        userId = argv[1]
+    user = sys.argv[1]
+    url_user = 'https://jsonplaceholder.typicode.com/users/' + user
+    res = requests.get(url_user)
+    """ANYTHING"""
+    user_name = res.json().get('username')
+    task = url_user + '/todos'
+    res = requests.get(task)
+    tasks = res.json()
 
-    employee_id = int(userId)
-    todo_url = '{}/todos?userId={}'.format(base_endpoint, employee_id)
-    username_url = '{}/users/{}'.format(base_endpoint, employee_id)
-
-    # make todo request
-    todo_response = requests.get(todo_url)
-    todo_task = todo_response.json()
-
-    # makae user request
-    username_response = requests.get(username_url)
-    username_json = username_response.json()
-
-    # User passed as an arg / todo task completed
-    NUMBER_OF_DONE_TASKS = 0
-    TOTAL_NUMBER_OF_TASKS = 0
-
-    TASK_TITLE = []
-
-    for item in todo_task:
-        count = 0
-        if 'completed' in item and item.get('completed') is True:
-            NUMBER_OF_DONE_TASKS += 1
-            TASK_TITLE.append(item.get('title'))
-        TOTAL_NUMBER_OF_TASKS += 1
-
-    # extract username
-    EMPLOYEE_NAME = username_json.get('name')
-
-    for element in TASK_TITLE:
-        print(" \t", element)
-
-    # export data in the CSV format
-    file_name = '{}.csv'.format(employee_id)
-
-    header = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
-              "TASK_TITLE"
-              ]
-    new_row = []
-
-    for task in todo_task:
-        new_row.append([employee_id, EMPLOYEE_NAME,
-                        task.get('completed'), task.get('title')])
-
-    with open(file_name, 'w') as employee_file:
-        csv_writer = csv.writer(employee_file, delimiter=',',
-                                quotechar='"', quoting=csv.QUOTE_ALL)
-        for task in new_row:
-            csv_writer.writerow(task)
+    with open('{}.csv'.format(user), 'w') as csvfile:
+        for task in tasks:
+            completed = task.get('completed')
+            """Complete"""
+            title_task = task.get('title')
+            """Done"""
+            csvfile.write('"{}","{}","{}","{}"\n'.format(
+                user, user_name, completed, title_task))
