7351a354f84ad2eb435cad5a7f5268440f2263d9
MahoneyT1 <ifeanyitech15@gmail.com>
2024-07-31 11:18:36 +0100

    wrote to a csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,63 +1,78 @@
 #!/usr/bin/python3
-'''
-gather employee data from API
-'''
 
-from sys import argv
+"""
+Script that uses JSONPlaceholder API to get information about an employee
+and exports TODO list to CSV.
+
+Requirements:
+- Records all tasks that are owned by this employee
+- Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+- File name must be: USER_ID.csv
+"""
+
 import csv
 import requests
+import sys
 
-base_endpoint = 'https://jsonplaceholder.typicode.com'
 
-if __name__ == '__main__':
-    if len(argv) <= 2:
-        userId = argv[1]
+def get_employee_info_and_tasks(employee_id):
+    """
+    Fetches information about an employee and their tasks from
+    JSONPlaceholder API.
 
-    employee_id = int(userId)
-    todo_url = '{}/todos?userId={}'.format(base_endpoint, employee_id)
-    username_url = '{}/users/{}'.format(base_endpoint, employee_id)
+    Args:
+    - employee_id (str): ID of the employee to retrieve information for.
 
-    # make todo request
-    todo_response = requests.get(todo_url)
-    todo_task = todo_response.json()
+    Returns:
+    - None
+    """
+    # JSONPlaceholder API base URL
+    api_url = 'https://jsonplaceholder.typicode.com/'
 
-    # makae user request
-    username_response = requests.get(username_url)
-    username_json = username_response.json()
+    # Construct URLs for user and tasks
+    user_url = '{}users/{}'.format(api_url, employee_id)
+    todos_url = '{}todos?userId={}'.format(api_url, employee_id)
+    # Fetch user information
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
 
-    # User passed as an arg / todo task completed
-    NUMBER_OF_DONE_TASKS = 0
-    TOTAL_NUMBER_OF_TASKS = 0
+    # Extract username
+    username = user_data.get('username')
 
-    TASK_TITLE = []
+    # Fetch tasks for the employee
+    todos_response = requests.get(todos_url)
+    tasks_data = todos_response.json()
 
-    for item in todo_task:
-        count = 0
-        if 'completed' in item and item.get('completed') is True:
-            NUMBER_OF_DONE_TASKS += 1
-            TASK_TITLE.append(item.get('title'))
-        TOTAL_NUMBER_OF_TASKS += 1
+    # Create a list of tasks with relevant information
+    tasks_list = []
+    for task in tasks_data:
+        tasks_list.append(
+            [employee_id, username, task.get('completed'), task.get('title')])
 
-    # extract username
-    EMPLOYEE_NAME = username_json.get('name')
+    # Create a CSV file with the employee's tasks
+    filename = '{}.csv'.format(employee_id)
+    with open(filename, mode='w') as employee_file:
+        employee_writer = csv.writer(
+            employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 
-    for element in TASK_TITLE:
-        print(" \t", element)
+        # Write header
+        # employee_writer.writerow(
+        # ['EmployeeID', 'Username', 'Completed', 'Title'])
 
-    # export data in the CSV format
-    file_name = '{}.csv'.format(employee_id)
+        # Write tasks data
+        for task in tasks_list:
+            employee_writer.writerow(task)
 
-    header = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
-              "TASK_TITLE"
-              ]
-    new_row = []
 
-    for task in todo_task:
-        new_row.append([employee_id, EMPLOYEE_NAME,
-                        task.get('completed'), task.get('title')])
+if __name__ == "__main__":
+    # Check if the correct number of command-line arguments is provided
+    if len(sys.argv) != 2:
+        print("Usage: python3 1-export_to_CSV.py <employee_id>")
+        sys.exit(1)
 
-    with open(file_name, 'w') as employee_file:
-        csv_writer = csv.writer(employee_file, delimiter=',',
-                                quotechar='"', quoting=csv.QUOTE_ALL)
-        for task in new_row:
-            csv_writer.writerow(task)
+    # Extract employee ID from command-line arguments
+    employee_id = sys.argv[1]
+
+    # Call the function to get employee information and tasks,
+    # and create a CSV file
+    get_employee_info_and_tasks(employee_id)
