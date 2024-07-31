3dbd5c8659b35cb53da4ba248396662eb620be17
MahoneyT1 <ifeanyitech15@gmail.com>
2024-07-31 03:32:53 +0100

    using rest api extracted data from different routes of te same app, and merged the datas together to form a complete dataset

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,49 +1,57 @@
 #!/usr/bin/python3
-"""a Python script that, using this REST API, for a
+"""A Python script that, using this REST API, for a
 given employee ID, returns information about his/her
-TODO list progress.
+Todo list progress.
 """
 import requests
 from sys import argv
 
 
-# extract employee_id from commandline
-employee_id = int(argv[1])
-base_endpoint = 'https://jsonplaceholder.typicode.com'
+userId = argv[1]
 
-if employee_id:
-    todo_url = '{}/todos?userId={}'.format(base_endpoint, employee_id)
-    username_url = '{}/users/{}'.format(base_endpoint, employee_id)
+def generate_data(userId):
+    """Gather data from different location from diff route"""
+    # extract employee_id from commandline
+    employee_id = int(userId)
+    base_endpoint = 'https://jsonplaceholder.typicode.com'
 
-    # make todo request
-    todo_response = requests.get(todo_url)
-    json_data = todo_response.json()
+    if employee_id:
+        todo_url = '{}/todos?userId={}'.format(base_endpoint, employee_id)
+        username_url = '{}/users/{}'.format(base_endpoint, employee_id)
 
-    # makae user request
-    username_response = requests.get(username_url)
-    username_json = username_response.json()
+        # make todo request
+        todo_response = requests.get(todo_url)
+        json_data = todo_response.json()
 
-    # User passed as an arg / todo task completed
-    NUMBER_OF_DONE_TASKS = 0
-    TOTAL_NUMBER_OF_TASKS = 0
+        # makae user request
+        username_response = requests.get(username_url)
+        username_json = username_response.json()
 
-    title = []
+        # User passed as an arg / todo task completed
+        NUMBER_OF_DONE_TASKS = 0
+        TOTAL_NUMBER_OF_TASKS = 0
 
-    for item in json_data:
-        count = 0
-        if 'completed' in item and item.get('completed') is True:
-            NUMBER_OF_DONE_TASKS += 1
-            title.append(item.get('title'))
-        TOTAL_NUMBER_OF_TASKS += 1
+        title = []
 
-    # extract username
-    EMPLOYEE_NAME = username_json.get('name')
+        for item in json_data:
+            count = 0
+            if 'completed' in item and item.get('completed') is True:
+                NUMBER_OF_DONE_TASKS += 1
+                title.append(item.get('title'))
+            TOTAL_NUMBER_OF_TASKS += 1
 
-    print("Employee {} is done with ({}/{}):".format(
-                                            EMPLOYEE_NAME,
-                                            NUMBER_OF_DONE_TASKS,
-                                            TOTAL_NUMBER_OF_TASKS
-                                            ))
+        # extract username
+        EMPLOYEE_NAME = username_json.get('name')
+
+        print("Employee {} is done with ({}/{}):".format(
+                                                EMPLOYEE_NAME,
+                                                NUMBER_OF_DONE_TASKS,
+                                                TOTAL_NUMBER_OF_TASKS
+                                                ))
 
     for element in title:
         print("\t", element)
+
+
+if __name__ == '__main__':
+    generate_data(userId=userId)
