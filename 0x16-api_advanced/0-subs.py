5280d0f5aac6c81f2bc924425759558e122c6290
MahoneyT1 <ifeanyitech15@gmail.com>
2024-08-14 13:35:36 +0100

    fix errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-"""Write a function that queries the Reddit API and returns
-the number of subscribers
+"""
+number of subscribers for a given subreddit
 """
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """Return total number of subscribers"""
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
 
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent':'0-subs.py/1.0'}
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
 
     try:
-        response = requests.get(url=url,
-                                headers=headers,
-                                allow_redirects=False)
+        return results.get('data').get('subscribers')
 
-        if response.status_code == 200:
-            response = response.json()
-            return response['data']['subscribers']
-        else:
-            return 0
-    except Exception as e:
+    except Exception:
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
-"""Write a function that queries the Reddit API and prints
-the titles of the first 10 hot posts listed for a given
-subreddit.
+
 """
-import requests
+prints the titles of the first 10 hot posts listed for a given subreddit
+"""
+
+from requests import get
 
 
 def top_ten(subreddit):
-    """prints the titles of the first 10 hot posts listed for
-    a given subreddit.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent':'1-top_ten.py'}
+    function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit
+    """
 
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
 
-    response = requests.get(url=url,
-                            headers=headers,
-                            allow_redirects=False)
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+
+    response = get(url, headers=user_agent, params=params)
+    results = response.json()
 
     try:
-        data = response.json()
-        if response.status_code == 200:
-            for post in data.get('data').get('children'):
-                print(post.get('data').get('title'))
-    except Exception as e:
-        print(None)
+        my_data = results.get('data').get('children')
+
+        for i in my_data:
+            print(i.get('data').get('title'))
+
+    except Exception:
+        print("None")
